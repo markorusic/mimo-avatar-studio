cd9cddbea6b4212a41f971b8222566ebbe6eed96
Marko Rusic <markorusic98@gmail.com>
2026-07-13 19:24:58 +0200

    Keep Sage avatar framework neutral

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,12 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: ["packages/sage-avatar/src/sage-avatar.tsx"],
+    rules: {
+      "@next/next/no-img-element": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
--- packages/sage-avatar/src/sage-avatar.tsx
+++ packages/sage-avatar/src/sage-avatar.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-/* eslint-disable @next/next/no-img-element -- This portable component cannot depend on next/image. */
-
 import {
   useEffect,
   useRef,
